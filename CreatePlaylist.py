95dcc721a92133e05a19ed8dde8074b926eafa65
rachelesgaroth <raquelsofiamcosta@gmail.com>
2020-09-01 15:40:04 +0100

    plano

---
+++ CreatePlaylist.py
@@ -0,0 +1,26 @@
+""""
+Step 1: Log into Youtube
+Step 2: Grab our liked videos
+Step 3: Create a new playlist
+Step 4: Search for the song
+Step 5: Add this song into the new Spotify playlist
+"""""
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    def get_youtube_client(self):
+        pass
+
+    def grab_liked_videos(self):
+        pass
+
+    def create_new_playlist(self):
+        pass
+
+    def search_song(self):
+        pass
+
+    def add_song(self):
+        pass
